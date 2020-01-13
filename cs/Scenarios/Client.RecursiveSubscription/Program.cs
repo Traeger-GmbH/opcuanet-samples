f684ec38dacd506d45ed67ecbbb49128918fac0c
Dominik Scharnagl <dominik.scharnagl@traeger.de>
2020-01-13 10:16:14 +0100

    Added Client.RecursiveSubscription and Server.RecursiveSubscription.

---
+++ cs/Scenarios/Client.RecursiveSubscription/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace RecursiveSubscription
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Client;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var client = new OpcClient("opc.tcp://localhost:4841")) {
+                client.Connect();
+                client.SubscribeEvent("ns=2;s=Machine/Jobs", HandleJobsEvent);
+
+                while (true) {
+                    Console.WriteLine("Press any key to schedule a new job.");
+                    Console.ReadLine();
+
+                    var jobsNode = client.BrowseNode("ns=2;s=Machine/Jobs");
+                    var job = $"JOB{GetLastJob(jobsNode) + 1:D2}";
+
+                    client.AddNode(new OpcAddDataVariableNode<int>(
+                            name: job,
+                            nodeId: OpcNodeId.Null,
+                            parentNodeId: jobsNode.NodeId,
+                            value: 0));
+
+                    Console.WriteLine($"CLIENT: New job '{job}' schedulded.");
+                }
+            }
+        }
+
+        private static int GetLastJob(OpcNodeInfo jobsNode)
+        {
+            var readJobs = new List<OpcReadNode>();
+
+            foreach (var childNode in jobsNode.Children()) {
+                if (childNode is OpcVariableNodeInfo jobNode)
+                    readJobs.Add(new OpcReadNode(jobNode.NodeId, OpcAttribute.BrowseName));
+            }
+
+            var client = jobsNode.Context.Client;
+
+            return (from value in client.ReadNodes(readJobs)
+                    where value.Status.IsGood
+                    let jobName = value.As<OpcName>().Value
+                    let jobId = int.Parse(jobName.Substring("JOB".Length))
+                    select jobId).Max();
+        }
+
+        private static void HandleJobsEvent(object sender, OpcEventReceivedEventArgs e)
+        {
+            Console.WriteLine("SERVER: {0} - {1}", e.Event.SourceName, e.Event.Message);
+        }
+    }
+}
---
+++ cs/Scenarios/Server.RecursiveSubscription/NodeManager.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace RecursiveSubscription
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Server;
+
+    internal class NodeManager : OpcNodeManager
+    {
+        #region ---------- Private readonly fields ----------
+
+        private readonly ConcurrentQueue<OpcDataVariableNode> jobs;
+
+        #endregion
+
+        #region ---------- Private fields ----------
+
+        private OpcFolderNode jobsNode;
+
+        private OpcEventNode jobScheduldedEventNode;
+        private OpcEventNode jobCompletedEventNode;
+
+        #endregion
+
+        #region ---------- Public constructors ----------
+
+        public NodeManager()
+            : base("http://sampleserver/recursivesubscription")
+        {
+            this.jobs = new ConcurrentQueue<OpcDataVariableNode>();
+        }
+
+        #endregion
+
+        #region ---------- Public methods ----------
+
+        public void Schedule(SemaphoreSlim semaphore)
+        {
+            var random = new Random();
+            var context = this.SystemContext;
+
+            while (!semaphore.Wait(3000)) {
+                if (this.jobs.TryDequeue(out var jobNode) && jobNode.Value is int stage) {
+                    this.jobScheduldedEventNode.ReportEventFrom(context, jobNode);
+
+                    while (!semaphore.Wait(500)) {
+                        stage = Math.Min(stage + random.Next(10), 100);
+
+                        jobNode.Timestamp = DateTime.UtcNow;
+                        jobNode.Value = stage;
+
+                        jobNode.ApplyChanges(context);
+
+                        if (stage == 100)
+                            break;
+                    }
+
+                    this.jobCompletedEventNode.ReportEventFrom(context, jobNode);
+                }
+            }
+        }
+
+        #endregion
+
+        #region ---------- Protected methods ----------
+
+        protected override void AddNode(
+                OpcContext context,
+                IOpcNode node,
+                IEnumerable<IOpcNodeReferenceAware> references)
+        {
+            base.AddNode(context, node, references);
+
+            if (node.Parent == this.jobsNode) {
+                if (node is OpcDataVariableNode jobNode)
+                    this.jobs.Enqueue(jobNode);
+            }
+        }
+
+        protected override IEnumerable<IOpcNode> CreateNodes(OpcNodeReferenceCollection references)
+        {
+            var machineNode = new OpcObjectNode(this.DefaultNamespace.GetName("Machine"));
+
+            this.jobsNode = new OpcFolderNode(machineNode, "Jobs");
+            new OpcDataVariableNode<int>(this.jobsNode, "JOB01", value: 0);
+            new OpcDataVariableNode<int>(this.jobsNode, "JOB02", value: 0);
+            new OpcDataVariableNode<int>(this.jobsNode, "JOB03", value: 0);
+
+            this.jobScheduldedEventNode = new OpcEventNode(this.jobsNode, "JobSchedulded");
+            this.jobScheduldedEventNode.Severity = OpcEventSeverity.Medium;
+            this.jobScheduldedEventNode.Message = "The job has been schedulded.";
+
+            this.jobsNode.AddNotifier(this.SystemContext, this.jobScheduldedEventNode);
+
+            this.jobCompletedEventNode = new OpcEventNode(this.jobsNode, "JobCompleted");
+            this.jobCompletedEventNode.Severity = OpcEventSeverity.Medium;
+            this.jobCompletedEventNode.Message = "The job has been completed.";
+
+            this.jobsNode.AddNotifier(this.SystemContext, this.jobCompletedEventNode);
+
+            references.Add(machineNode, OpcObjectTypes.ObjectsFolder);
+            return new IOpcNode[] { machineNode };
+        }
+
+        #endregion
+    }
+}
