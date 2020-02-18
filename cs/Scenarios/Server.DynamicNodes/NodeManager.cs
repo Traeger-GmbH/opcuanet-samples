a0f95c36154fc26c1554731c049d582d011e59b0
d.scharnagl <d.scharnagl@traeger.de>
2020-02-18 17:20:02 +0100

    Added new scenario 'Server.DynamicNodes'.

---
+++ cs/Scenarios/Server.DynamicNodes/NodeManager.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace DynamicNodes
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Server;
+
+    internal partial class NodeManager : OpcNodeManager
+    {
+        #region ---------- Private readonly fields ----------
+
+        private readonly OpcFolderNode rootNode;
+
+        #endregion
+
+        #region ---------- Public constructors ----------
+
+        public NodeManager()
+            : base("http://sampleserver/dynamicnodes")
+        {
+            this.rootNode = new OpcFolderNode(this.DefaultNamespace.GetName("Root"));
+        }
+
+        #endregion
+
+        #region ---------- Internal methods ----------
+
+        internal IOpcNode AddNode(params string[] path)
+        {
+            var context = this.SystemContext;
+            var parentNode = (OpcInstanceNode)this.rootNode;
+
+            foreach (var name in path) {
+                var childNode = (OpcInstanceNode)parentNode.Child(context, name);
+
+                if (childNode == null) {
+                    if (name[0] == '.')
+                        childNode = new OpcDataVariableNode<int>(parentNode, name, DateTime.Now.Millisecond);
+                    else
+                        childNode = new OpcFolderNode(parentNode, name);
+
+                    this.AddNode(context, childNode);
+
+                    if (childNode is OpcVariableNode)
+                        return childNode;
+                }
+
+                parentNode = childNode;
+            }
+
+            return parentNode;
+        }
+
+        #endregion
+
+        #region ---------- Protected methods ----------
+
+        protected override IEnumerable<IOpcNode> CreateNodes(OpcNodeReferenceCollection references)
+        {
+            references.Add(this.rootNode, OpcObjectTypes.ObjectsFolder);
+            return new IOpcNode[] { this.rootNode };
+        }
+
+        #endregion
+    }
+}
---
+++ cs/Scenarios/Server.DynamicNodes/Program.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace DynamicNodes
+{
+    using System;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Server;
+
+    /// <summary>
+    /// This use case realizes dynamic nodes which are created on-demand by the server depending
+    /// on the underlying system needs.
+    /// </summary>
+    public class Program
+    {
+        #region ---------- Public static methods ----------
+
+        public static void Main(string[] args)
+        {
+            var manager = new NodeManager();
+
+            using (var server = new OpcServer("opc.tcp://localhost:4840/", manager)) {
+                server.Start();
+
+                Console.WriteLine("Press enter to exit or ...");
+                Console.WriteLine("... enter a node path like '/a/b/c' to create a folder node.");
+                Console.WriteLine("... enter a path like '/a/b/.d' to create a numeric variable node.");
+                Console.WriteLine();
+
+                while (true) {
+                    var path = Console.ReadLine().Trim().Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (path.Length == 0)
+                        break;
+
+                    var node = manager.AddNode(path);
+                    Console.WriteLine("Created: '{0}'", node.Id.ToString(OpcNodeIdFormat.Foundation));
+                }
+            }
+        }
+    }
+
+    #endregion
+}
