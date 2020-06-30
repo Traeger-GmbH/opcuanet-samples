ac77fe307947e0b63691d1d9da298d4b2b920387
d.scharnagl <d.scharnagl@traeger.de>
2020-06-30 08:05:49 +0200

    Added Client.NodeExplorer for VB.NET

---
+++ vb/Scenarios/Client.NodeExplorer/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+   'Form overrides dispose to clean up the component list.
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso (components IsNot Nothing) Then
+            components.Dispose()
+        End If
+
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.nodesImageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.tableLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+        Me.serverAddressTextBox = New System.Windows.Forms.TextBox()
+        Me.connectButton = New System.Windows.Forms.Button()
+        Me.nodesTreeView = New System.Windows.Forms.TreeView()
+        Me.tableLayoutPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'nodesImageList
+        '
+        Me.nodesImageList.ImageStream = CType(resources.GetObject("nodesImageList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.nodesImageList.TransparentColor = System.Drawing.Color.Transparent
+        Me.nodesImageList.Images.SetKeyName(0, "ObjectNode.png")
+        Me.nodesImageList.Images.SetKeyName(1, "FolderNode.png")
+        Me.nodesImageList.Images.SetKeyName(2, "MethodNode.png")
+        Me.nodesImageList.Images.SetKeyName(3, "VariableNode.png")
+        Me.nodesImageList.Images.SetKeyName(4, "PropertyNode.png")
+        '
+        'tableLayoutPanel
+        '
+        Me.tableLayoutPanel.ColumnCount = 2
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tableLayoutPanel.Controls.Add(Me.serverAddressTextBox, 0, 0)
+        Me.tableLayoutPanel.Controls.Add(Me.connectButton, 1, 0)
+        Me.tableLayoutPanel.Controls.Add(Me.nodesTreeView, 0, 1)
+        Me.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tableLayoutPanel.Location = New System.Drawing.Point(0, 0)
+        Me.tableLayoutPanel.Name = "tableLayoutPanel"
+        Me.tableLayoutPanel.RowCount = 2
+        Me.tableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tableLayoutPanel.Size = New System.Drawing.Size(334, 561)
+        Me.tableLayoutPanel.TabIndex = 0
+        '
+        'serverAddressTextBox
+        '
+        Me.serverAddressTextBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.serverAddressTextBox.Location = New System.Drawing.Point(3, 4)
+        Me.serverAddressTextBox.Name = "serverAddressTextBox"
+        Me.serverAddressTextBox.Size = New System.Drawing.Size(247, 20)
+        Me.serverAddressTextBox.TabIndex = 0
+        Me.serverAddressTextBox.Text = "opc.tcp://localhost:4840"
+        '
+        'connectButton
+        '
+        Me.connectButton.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.connectButton.Location = New System.Drawing.Point(256, 3)
+        Me.connectButton.Name = "connectButton"
+        Me.connectButton.Size = New System.Drawing.Size(75, 23)
+        Me.connectButton.TabIndex = 1
+        Me.connectButton.Text = "Connect"
+        Me.connectButton.UseVisualStyleBackColor = True
+        '
+        'nodesTreeView
+        '
+        Me.tableLayoutPanel.SetColumnSpan(Me.nodesTreeView, 2)
+        Me.nodesTreeView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.nodesTreeView.ImageIndex = 0
+        Me.nodesTreeView.ImageList = Me.nodesImageList
+        Me.nodesTreeView.Location = New System.Drawing.Point(3, 32)
+        Me.nodesTreeView.Name = "nodesTreeView"
+        Me.nodesTreeView.SelectedImageIndex = 0
+        Me.nodesTreeView.Size = New System.Drawing.Size(328, 526)
+        Me.nodesTreeView.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(334, 561)
+        Me.Controls.Add(Me.tableLayoutPanel)
+        Me.MinimumSize = New System.Drawing.Size(350, 600)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Client.NodeExplorer"
+        Me.tableLayoutPanel.ResumeLayout(False)
+        Me.tableLayoutPanel.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private nodesImageList As System.Windows.Forms.ImageList
+    Private tableLayoutPanel As System.Windows.Forms.TableLayoutPanel
+    Private serverAddressTextBox As System.Windows.Forms.TextBox
+    Private WithEvents connectButton As System.Windows.Forms.Button
+    Private WithEvents nodesTreeView As System.Windows.Forms.TreeView
+End Class
---
+++ vb/Scenarios/Client.NodeExplorer/Form1.vb
@@ -0,0 +1,112 @@
+﻿' Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+Imports Opc.UaFx
+Imports Opc.UaFx.Client
+
+Public Partial Class Form1
+    Private ReadOnly client As OpcClient
+
+    Public Sub New()
+        MyBase.New()
+        Me.client = New OpcClient()
+        Me.InitializeComponent()
+    End Sub
+
+    Protected Overrides Sub OnFormClosing(ByVal e As FormClosingEventArgs)
+        Me.client.Disconnect()
+        MyBase.OnFormClosing(e)
+    End Sub
+
+    Private Function Browse() As Boolean
+        Dim result = False
+
+        Try
+            Dim node = Me.client.BrowseNode(OpcObjectTypes.RootFolder)
+            result = Me.Browse(node)
+        Catch ex As OpcException
+            Me.ShowMessage("Browse", "Failed to browse: " & ex.Message)
+        End Try
+
+        Return result
+    End Function
+
+    Private Function Browse(ByVal node As OpcNodeInfo) As Boolean
+        Me.nodesTreeView.Nodes.Clear()
+        Return Me.Browse(node, Me.nodesTreeView.Nodes)
+    End Function
+
+    Private Function Browse(
+            ByVal node As OpcNodeInfo,
+            ByVal treeNodes As TreeNodeCollection) As Boolean
+        Dim result = False
+
+        Try
+            Dim treeNode = treeNodes.Add(node.DisplayName.Value)
+
+            If TypeOf node Is OpcObjectNodeInfo Then
+                treeNode.ImageIndex = 0
+                If node.Reference.TypeDefinitionId = Opc.Ua.ObjectTypeIds.FolderType Then
+                    treeNode.ImageIndex = 1
+                End If
+            ElseIf TypeOf node Is OpcMethodNodeInfo Then
+                treeNode.ImageIndex = 2
+            ElseIf TypeOf node Is OpcVariableNodeInfo Then
+                treeNode.ImageIndex = 3
+                If node.Reference.ReferenceType = OpcReferenceType.HasProperty Then
+                    treeNode.ImageIndex = 4
+                End If
+            End If
+
+            treeNode.Tag = node
+            treeNode.Nodes.Add("Browsing...")
+
+            result = True
+        Catch ex As OpcException
+            Me.ShowMessage("Browse", "Failed to browse: " & ex.Message)
+        End Try
+
+        Return result
+    End Function
+
+    Private Function Connect() As Boolean
+        Dim result = False
+
+        Try
+            Me.client.Connect()
+            result = True
+        Catch ex As OpcException
+            Me.ShowMessage("Connect", "Failed to connect: " & ex.Message)
+        End Try
+
+        Return result
+    End Function
+
+    Private Sub HandleConnectButtonClick(ByVal sender As Object, ByVal e As EventArgs) Handles connectButton.Click
+        Me.client.Disconnect()
+        Dim serverAddress As Uri = Nothing
+
+        If Uri.TryCreate(Me.serverAddressTextBox.Text, UriKind.Absolute, serverAddress) Then
+            Me.client.ServerAddress = serverAddress
+            If Me.Connect() Then Me.Browse()
+        Else
+            Me.ShowMessage("Connect", "Invalid server address.")
+        End If
+    End Sub
+
+    Private Sub HandleNodesTreeViewAfterExpand(ByVal sender As Object, ByVal e As TreeViewEventArgs) Handles nodesTreeView.AfterExpand
+        Dim node As OpcNodeInfo = TryCast(e.Node.Tag, OpcNodeInfo)
+
+        If node IsNot Nothing Then
+            Dim treeNodes = e.Node.Nodes
+            treeNodes.Clear()
+
+            For Each childNode In node.Children()
+                If Not Me.Browse(childNode, treeNodes) Then Exit For
+            Next
+        End If
+    End Sub
+
+    Private Sub ShowMessage(ByVal caption As String, ByVal text As String)
+        MessageBox.Show(owner:=Me, text, caption, MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
