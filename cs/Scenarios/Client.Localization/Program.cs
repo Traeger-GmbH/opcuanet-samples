9f88483772e52e95fd5ed3f44fd00fe815112152
d.scharnagl <d.scharnagl@traeger.de>
2020-11-23 15:24:14 +0100

    Introduced new Client/Server sample for localization.

---
+++ cs/Scenarios/Client.Localization/Program.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace Localization
+{
+    using System;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Client;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //// #Starting Notes
+            //// * The Server always tries to offer localized information using the locales preferred by the client.
+            //// * The Server always falls back to its default localization in case a requested local is not supported.
+            //// * The locale strings used are the ISO normalized culture names used by the CultureInfo.
+            //// * The use of multiple locales results into a prioritization by its order and the Server provides the
+            ////   localization for the first locale preferred and supported.
+
+            // 1. Case: Use the current thread culture (see CultureInfo.CurrentCulture).
+            using (var client = new OpcClient("opc.tcp://localhost:4840")) {
+                client.Connect();
+
+                var displayName = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.DisplayName);
+                var description = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.Description);
+
+                Console.WriteLine("{0} - {1}", displayName, description);
+            }
+
+            // 2. Case: Use an explict culture e.g. 'fr' for France.
+            using (var client = new OpcClient("opc.tcp://localhost:4840")) {
+                client.Locales.Add("fr");
+                client.Connect();
+
+                var displayName = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.DisplayName);
+                var description = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.Description);
+
+                Console.WriteLine("{0} - {1}", displayName, description);
+            }
+
+            // 3. Case: Use an explict culture e.g. 'de' for German.
+            using (var client = new OpcClient("opc.tcp://localhost:4840")) {
+                client.Locales.Add("de");
+                client.Connect();
+
+                var displayName = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.DisplayName);
+                var description = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.Description);
+
+                Console.WriteLine("{0} - {1}", displayName, description);
+            }
+
+            // 4. Case: Use multiple cultures. If a localized information can not offered for the
+            //          first the second culture is used instead.
+            using (var client = new OpcClient("opc.tcp://localhost:4840")) {
+                client.Locales.Add("it"); // 1st we prefer "Italian".
+                client.Locales.Add("en"); // 2nd if "Italian" is not supported: Give us "English".
+                client.Connect();
+
+                var displayName = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.DisplayName);
+                var description = client.ReadNode("ns=2;s=MyFolder", OpcAttribute.Description);
+
+                Console.WriteLine("{0} - {1}", displayName, description);
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ cs/Scenarios/Server.Localization/Program.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace Localization
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Server;
+
+    /// <summary>
+    /// This use case realizes restricted node access using built-in ACL functionality and
+    /// subclassing for user dependent node metadata.
+    /// </summary>
+    public class Program
+    {
+        #region ---------- Public static methods ----------
+
+        public static void Main(string[] args)
+        {
+            var node = new OpcFolderNode("MyFolder");
+
+            node.DisplayName = new OpcText("MyFolder", "en-US", "MyFolder.DisplayName");
+            node.Description = new OpcText("MyFolder Description", "en-US", "MyFolder.Description");
+
+            using (var server = new OpcServer("opc.tcp://localhost:4840", node)) {
+                server.Globalization.AddResources(
+                        "en-US",
+                        new KeyValuePair<string, string>("MyFolder.DisplayName", "MyFolder (en)"),
+                        new KeyValuePair<string, string>("MyFolder.Description", "MyFolder Description (en)"));
+                server.Globalization.AddResources(
+                        "de-DE",
+                        new KeyValuePair<string, string>("MyFolder.DisplayName", "Mein Ordner (de)"),
+                        new KeyValuePair<string, string>("MyFolder.Description", "Meine Ordner Beschreibung (de)"));
+                server.Globalization.AddResources(
+                        "fr-FR",
+                        new KeyValuePair<string, string>("MyFolder.DisplayName", "Mon dossier (fr)"),
+                        new KeyValuePair<string, string>("MyFolder.Description", "Description de mon dossier (fr)"));
+
+                server.Start();
+
+                Console.WriteLine("Server started - press any key to exit.");
+                Console.ReadKey(true);
+            }
+        }
+
+        #endregion
+    }
+}
