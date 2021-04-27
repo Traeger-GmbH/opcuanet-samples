9cf4580048e61e320cbfde5ec71618c555c02eb0
d.scharnagl <d.scharnagl@traeger.de>
2021-04-27 09:38:44 +0200

    Introduced sample for data changes with high resolution (= many changes in short time).

---
+++ cs/Subscriptions/Client.BulkChanges.HiRes/Program.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace BulkChanges
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Threading;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Client;
+
+    [SuppressMessage("Style", "IDE1006:Naming Styles")]
+    public class Program
+    {
+        #region ---------- Private static fields ----------
+
+        private static CancellationTokenSource consumerControl;
+
+        private static BlockingCollection<OpcValue[]> dataChanges;
+        private static OpcSubscription dataChangeSubscription;
+
+        #endregion
+
+        #region ---------- Public static methods ----------
+
+        public static void Main()
+        {
+            consumerControl = new CancellationTokenSource();
+            dataChanges = new BlockingCollection<OpcValue[]>();
+
+            var consumer = new Thread(ConsumeDataChanges);
+
+            using (var client = new OpcClient("opc.tcp://localhost:4840")) {
+                client.Connect();
+                consumer.Start(client);
+
+                Console.WriteLine("Client started - press any key to exit.");
+                Console.ReadKey(true);
+
+                consumerControl.Cancel();
+                consumer.Join();
+            }
+        }
+
+        #endregion
+
+        #region ---------- Private static methods ----------
+
+        private static IEnumerable<OpcMonitoredItem> CreateMonitoredItems(OpcClient client, OpcNodeId rootNodeId)
+        {
+            var node = client.BrowseNode(rootNodeId);
+
+            foreach (var childNode in node.Children())
+                yield return new OpcMonitoredItem(childNode.NodeId, OpcAttribute.Value);
+        }
+
+        private static void HandleDataChange(IEnumerable<OpcDataChangeDataSetItem> dataChangeItems)
+        {
+            // Determine the according monitored item for each data change.
+            ////var monitoredItem = dataChangeSubscription.GetMonitoredItem(dataChangeItem);
+
+            dataChanges.Add(dataChangeItems.Select(item => item.Value).ToArray());
+        }
+
+        private static void ConsumeDataChanges(object state)
+        {
+            var client = (OpcClient)state;
+            var subscription = dataChangeSubscription = client.SubscribeNodes();
+
+            subscription.AddMonitoredItem(CreateMonitoredItems(client, "ns=2;s=Data"));
+            subscription.ReceivedDataChangeCallback = HandleDataChange;
+
+            // Enforce the fastest supported publishing interval.
+            subscription.PublishingInterval = 0;
+
+            // Disable monitored item related cache und data change events.
+            subscription.UseMonitoredItemDataCache = false;
+
+            // Commit recent changes to the subscription.
+            subscription.ApplyChanges();
+
+            while (!consumerControl.IsCancellationRequested) {
+                try {
+                    var values = dataChanges.Take(consumerControl.Token);
+
+                    for (int index = 0; index < values.Length; index++) {
+                        var value = values[index];
+
+                        if (value.Value is DateTime timestamp) {
+                            Console.WriteLine(
+                                    "{0} BULK: Completed (Duration = {1} ms)",
+                                    DateTime.Now,
+                                    DateTime.UtcNow.Subtract(timestamp).TotalMilliseconds);
+                        }
+                        else {
+                            Console.WriteLine(
+                                    "{0} BULK: New Data: {1}",
+                                    DateTime.Now,
+                                    value);
+                        }
+                    }
+                }
+                catch (OperationCanceledException) {
+                    break;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ cs/Subscriptions/Server.BulkChanges.HiRes/Program.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace BulkChanges
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Server;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SuppressMessage("Style", "IDE1006:Naming Styles")]
+    public class Program
+    {
+        #region ---------- Private static readonly fields ----------
+
+        private static readonly int Interval = 5;
+        private static readonly int NumberOfNodes = 500;
+
+        #endregion
+
+        #region ---------- Private static fields ----------
+
+        private static CancellationTokenSource producerControl;
+
+        private static OpcFolderNode dataNode;
+        private static List<OpcDataVariableNode<int>> dataNodes;
+
+        private static OpcDataVariableNode<DateTime> timestampNode;
+
+        #endregion
+
+        #region ---------- Public static methods ----------
+
+        public static void Main(string[] args)
+        {
+            producerControl = new CancellationTokenSource();
+
+            dataNodes = new List<OpcDataVariableNode<int>>();
+            dataNode = CreateDataNode();
+
+            var producer = new Thread(ProduceDataChanges);
+
+            using (var server = new OpcServer("opc.tcp://localhost:4840/", dataNode)) {
+                server.Configuration.DisableHiResClock = false;
+                server.Configuration.ServerConfiguration.PublishingResolution = 1;
+                server.Configuration.ServerConfiguration.MinPublishingInterval = 5;
+
+                server.Start();
+                producer.Start(server);
+
+                Console.WriteLine("Server started - press any key to exit.");
+                Console.ReadKey(true);
+
+                producerControl.Cancel();
+                producer.Join();
+            }
+        }
+
+        #endregion
+
+        #region ---------- Private static methods ----------
+
+        private static OpcFolderNode CreateDataNode()
+        {
+            var dataNode = new OpcFolderNode("Data");
+
+            for (int index = 0; index < NumberOfNodes; index++)
+                dataNodes.Add(new OpcDataVariableNode<int>(dataNode, $"Var{index:00000}", value: index));
+
+            timestampNode = new OpcDataVariableNode<DateTime>(dataNode, "Timestamp");
+            return dataNode;
+        }
+
+        private static void ProduceDataChanges(object state)
+        {
+            var server = (OpcServer)state;
+
+            while (!producerControl.IsCancellationRequested) {
+                lock (state) {
+                    unchecked {
+                        foreach (var node in dataNodes)
+                            node.Value++;
+                    }
+
+                    timestampNode.Value = DateTime.UtcNow;
+                    dataNode.ApplyChanges(server.SystemContext, recursive: true);
+                }
+
+                Console.WriteLine("{0} BULK: New Data available - pausing for {1} ms.", DateTime.Now, Interval);
+
+                // Wait for "Stop" or perform next bulk changes after interval elapsed
+                if (producerControl.Token.WaitHandle.WaitOne(Interval))
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
