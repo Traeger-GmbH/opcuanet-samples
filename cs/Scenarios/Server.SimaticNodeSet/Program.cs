61aa10d55b2e09e3ddb65678eda3bab789ae7337
d.scharnagl <d.scharnagl@traeger.de>
2022-02-11 17:19:17 +0100

    Introduced new Server.SimaticNodeSet sample which demonstrates how a simulation approach of SIMATIC S7 OPC UA Servers information model can be implemented (this is just a frame) using an exported UANodeSet.

---
+++ cs/Scenarios/Server.SimaticNodeSet/Program.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace SimaticNodeSet
+{
+    using System;
+    using Opc.UaFx.Server;
+
+    /// <summary>
+    /// This use case demonstrates how to define a custom server which can be used to simulate a
+    /// "SIEMENS SIMATIC OPC UA Server" using a once exported information model (= nodeset).
+    /// </summary>
+    /// <remarks>This implementation provides only a frame to start with the development.</remarks>
+    public class Program
+    {
+        #region ---------- Public static methods ----------
+
+        public static void Main(string[] args)
+        {
+            var nodeManager = new NodeManager();
+
+            using (var server = new OpcServer("opc.tcp://localhost:4840/", nodeManager)) {
+                server.ApplicationUri = new Uri("urn:SIMATIC.S7-1500.OPC-UA.Application:PLC_1");
+                server.Start();
+
+                nodeManager.Start();
+
+                Console.WriteLine("Server started - press any key to exit.");
+                Console.ReadKey(true);
+            }
+        }
+
+        #endregion
+    }
+}
