ebddd750cb82028e77eec8240762e76d6afd22bb
d.scharnagl <d.scharnagl@traeger.de>
2022-03-04 11:33:39 +0100

    Introduced new sample to represent how to handle events in a generic way.

---
+++ cs/Scenarios/Client.GenericEvents/GenericEvent.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace GenericEvents
+{
+    using System.Collections.Generic;
+    using Opc.UaFx;
+
+    internal class GenericEvent : OpcEvent
+    {
+        public GenericEvent(IOpcReadOnlyNodeDataStore dataStore)
+            : base(dataStore)
+        {
+        }
+
+        public Dictionary<string, object> GetData()
+        {
+            var data = default(Dictionary<string, object>);
+
+            if (this.DataStore is OpcEventNodeView eventView) {
+                var operands = eventView.Filter.SelectClause;
+                var fields = eventView.Data.Fields;
+
+                data = new Dictionary<string, object>();
+
+                for (int index = 0; index < operands.Count; index++) {
+                    var operand = operands[index];
+                    var value = new OpcValue(fields[index].Value).Value;
+
+                    data.Add(operand.NodePath.ToString(), value);
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ cs/Scenarios/Client.GenericEvents/Program.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
+
+namespace GenericEvents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Opc.UaFx;
+    using Opc.UaFx.Client;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            OpcEvent.RegisterType<GenericEvent>("ns=2;i=1");
+
+            using (var client = new OpcClient("opc.tcp://localhost:4840")) {
+                client.UseDynamic = true;
+                client.Connect();
+
+                // Define a filter to only handle the custom event.
+                var filter = OpcFilter.Using(client)
+                        .FromEvents(OpcNodeId.Parse("ns=2;i=1"))
+                        .Select();
+
+                var sub = client.SubscribeEvent("ns=2;s=Machine", filter, (sender, e) => {
+                    if (e.Event is GenericEvent genericEvent)
+                        Print(genericEvent.GetData());
+                });
+
+                Console.Write("Waiting for events (press any key to exit)...");
+                Console.ReadKey(true);
+            }
+        }
+
+        private static void Print(Dictionary<string, object> data, int level = 0)
+        {
+            var indent = new string('.', level * 2);
+
+            foreach (var item in data) {
+                if (item.Value is OpcDataObject instance) {
+                    Console.WriteLine(indent + item.Key);
+
+                    var instanceData = instance.GetFields().ToDictionary(
+                            field => field.Name,
+                            field => field.Value);
+
+                    Print(instanceData, level + 1);
+                }
+                else if (item.Value is OpcDataObject[] instances) {
+                    for (int index = 0; index < instances.Length; index++) {
+                        Console.WriteLine(indent + "{0}[{1}]", item.Key, index);
+
+                        var instanceData = instances[index].GetFields().ToDictionary(
+                                field => field.Name,
+                                field => field.Value);
+
+                        Print(instanceData, level + 1);
+                    }
+                }
+                else {
+                    Console.WriteLine(indent + "{0} = {1}", item.Key, item.Value);
+                }
+            }
+        }
+    }
+}
