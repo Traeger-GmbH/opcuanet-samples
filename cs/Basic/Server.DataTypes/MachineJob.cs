d311216de88f17b1bf14ca4584310d362a017a7a
kpreisser <kpreisser@users.noreply.github.com>
2026-06-30 16:04:44 +0200

    Fix the Server.DataType sample to use `OpcEncodingMaskKind.Explicit`, so that the data type is compatible with both the `DataTypeDefinition` attribute as well as the Type Dictionary.

--- cs/Basic/Server.DataTypes/MachineJob.cs
+++ cs/Basic/Server.DataTypes/MachineJob.cs
@@ -3,28 +3,46 @@
 namespace DataTypes
 {
     using System;
+
     using Opc.UaFx;
 
     // Use the 'OpcDataTypeAttribute' to declare the type as a data type used in OPC UA as well.
     [OpcDataType("ns=2;s=MachineJob")]
     // Use the 'OpcDataTypeEncodingAttribute' to declare the type specific encoding of the data type.
     [OpcDataTypeEncoding("ns=2;s=MachineJob.Binary", Type = OpcEncodingType.Binary)]
+    // Use 'OpcEncodingMaskKind.Explicit' to enable the use of optional fields that are
+    // compatible with both the DataTypeDefinition attribute and the Type Dictionary.
+    [OpcDataTypeEncodingMask(OpcEncodingMaskKind.Explicit)]
     public class MachineJob
     {
+        // Using optional fields with OpcEncodingMaskKind.Explicit requires to declare a
+        // Opc.UaFx.Bit field for each optional field at the beginning of the data type, where
+        // the order of the Bit fields must correspond to the order of the optional fields.
+        public Bit DurationSpecified
+        {
+            get => this.Duration != null;
+            set => this.Duration = value ? (this.Duration ?? 0) : null;
+        }
+
+        // Additionally, a Bit array must be declared that reserves the (unused)
+        // remaining bits of the 32-bit field.
+        [OpcDataTypeMemberLength(31U)]
+        public Bit[] Reserved1
+        {
+            get;
+            set;
+        }
+
         public string Number
         {
             get;
             set;
         }
 
-        public bool DurationSpecified
-        {
-            get => this.Duration.HasValue;
-            set => this.Duration = value ? 0 : (int?)null;
-        }
-
-        // Use the 'OpcDataTypeMemberSwitch' to discard a field depending on the value
-        // of another field.
+        // Use the 'OpcDataTypeMemberSwitchAttribute' to declare an optional field, so its
+        // presence depends on the value of another field.
+        // For compatibility with the DataTypeDefinition attribute, the first optional field
+        // must refer to the first Bit field, and so on.
         [OpcDataTypeMemberSwitch(nameof(DurationSpecified))]
         public int? Duration
         {
