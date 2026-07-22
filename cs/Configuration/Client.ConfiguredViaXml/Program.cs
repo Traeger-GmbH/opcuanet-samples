7dd520d1aa4f1e8293c44f50a92afbc6618412a1
kpreisser <kpreisser@users.noreply.github.com>
2026-07-22 09:36:44 +0200

    Update samples to no longer load a configuration file from an App.Config section, as that does not work in the current .NET Standard based stack.
    Fixes #5

--- cs/Configuration/Client.ConfiguredViaXml/Program.cs
+++ cs/Configuration/Client.ConfiguredViaXml/Program.cs
@@ -23,15 +23,9 @@ namespace ConfiguredViaXml
             //// By default it is not necessary to explicitly configure an OPC UA client. But in case
             //// of advanced and productive scenarios you will have to.
 
-            // There are different ways to load the client configuration.
-            OpcApplicationConfiguration configuration = null;
-
-            // 1st Way: Load client config using a file path.
-            configuration = OpcApplicationConfiguration.LoadClientConfigFile(
+            // Load client config using a file path.
+            var configuration = OpcApplicationConfiguration.LoadClientConfigFile(
                     Path.Combine(Environment.CurrentDirectory, "ClientConfig.xml"));
-
-            // 2nd Way: Load client config specified in a specific section of your App.config.
-            configuration = OpcApplicationConfiguration.LoadClientConfig("Opc.UaFx.Client");
 
             // If the client uris domain name does not match localhost just replace it
             // e.g. with the IP address or name of the client machine.
@@ -43,14 +37,11 @@ namespace ConfiguredViaXml
             client.Connect();
             client.Disconnect();
 
-            // In case you are using the OpcClientApplication class, you can explicitly trigger
-            // loading a configuration file using the App.config as the following code does
-            // demonstrate.
+            // In case you are using the OpcClientApplication class, you can assign the
+            // loaded client configuration on the client instance used by the application instance,
+            // as the following code demonstrates.
             var app = new OpcClientApplication("opc.tcp://localhost:4840/SampleClient");
-            app.LoadConfiguration();
 
-            // Alternatively you can assign the manually loaded client configuration on the client
-            // instance used by the application instance, as the following code does demonstrate.
             app.Client.Configuration = configuration;
 
             app.Run();
--- cs/Configuration/Server.ConfiguredViaXml/Program.cs
+++ cs/Configuration/Server.ConfiguredViaXml/Program.cs
@@ -23,15 +23,9 @@ namespace ConfiguredViaXml
             //// By default it is not necessary to explicitly configure an OPC UA server. But in case
             //// of advanced and productive scenarios you will have to.
 
-            // There are different ways to load the server configuration.
-            OpcApplicationConfiguration configuration = null;
-
-            // 1st Way: Load server config using a file path.
-            configuration = OpcApplicationConfiguration.LoadServerConfigFile(
+            // Load server config using a file path.
+            var configuration = OpcApplicationConfiguration.LoadServerConfigFile(
                     Path.Combine(Environment.CurrentDirectory, "ServerConfig.xml"));
-
-            // 2nd Way: Load server config specified in a specific section of your App.config.
-            configuration = OpcApplicationConfiguration.LoadServerConfig("Opc.UaFx.Server");
 
             // If the server domain name does not match localhost just replace it
             // e.g. with the IP address or name of the server machine.
@@ -45,17 +39,13 @@ namespace ConfiguredViaXml
             server.Start();
             server.Stop();
 
-            // In case you are using the OPC UA server (Service) Application class, you can explicitly
-            // trigger loading a configuration file using the App.config as the following code does
-            // demonstrate.
+            // In case you are using the OPC UA server (Service) Application class, you can assign
+            // the loaded server configuration on the server instance used by the application instance,
+            // as the following code demonstrates.
             var app = new OpcServerApplication(
                     "opc.tcp://localhost:4840/SampleServer",
                     new SampleNodeManager());
 
-            app.LoadConfiguration();
-
-            // Alternatively you can assign the manually loaded server configuration on the server
-            // instance used by the application instance, as the following code does demonstrate.
             app.Server.Configuration = configuration;
 
             app.Run();
